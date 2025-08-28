f50d823aeeba6bfa59f6db6a78ff51edfff9291f
Jonas <Jonas0504hansen@gmail.com>
2025-08-28 11:31:44 +0200

    Fil uden warnings

--- Main.java
+++ Main.java
@@ -2,17 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Opret Scanner objekt
-        Scanner input = new Scanner(System.in);
         // Uskriver Navn
-        System.out.println("Skriv venligst dit fornavn");
-        // Bed brugeren om at indtaste sit navn
-        String name = input.nextLine();
-        // Udskriv navnet
-        System.out.println("Dit fornavn er: " + name);
-        System.out.println("Skriv venligst dit efternavn");
-        String nameLast = input.nextLine();
-        System.out.println("Dit efternavn er: " + nameLast);
-        System.out.println("Dit fulde navn er: " + name + " " + nameLast);
+        try ( // Opret Scanner objekt
+                Scanner input = new Scanner(System.in)) {
+            // Uskriver Navn
+            System.out.println("Skriv venligst dit fornavn");
+            // Bed brugeren om at indtaste sit navn
+            String name = input.nextLine();
+            // Udskriv navnet
+            System.out.println("Dit fornavn er: " + name);
+            System.out.println("Skriv venligst dit efternavn");
+            String nameLast = input.nextLine();
+            System.out.println("Dit efternavn er: " + nameLast);
+            System.out.println("Dit fulde navn er: " + name + " " + nameLast);
+        }
     }
 } 
