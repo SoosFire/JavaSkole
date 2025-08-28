76b89833feab785ea60a89e2a491af6a4177b37d
Jonas <Jonas0504hansen@gmail.com>
2025-08-28 11:27:33 +0200

    Ny fil

--- Main.java
+++ Main.java
@@ -5,11 +5,12 @@ public class Main {
         // Opret Scanner objekt
         Scanner input = new Scanner(System.in);
         // Uskriver Navn
-        System.out.println("Jonas Hansen");
+        System.out.println("Skriv venligst dit fornavn");
         // Bed brugeren om at indtaste sit navn
         String name = input.nextLine();
         // Udskriv navnet
-        System.out.println("Dit navn er: " + name);
+        System.out.println("Dit fornavn er: " + name);
+        System.out.println("Skriv venligst dit efternavn");
         String nameLast = input.nextLine();
         System.out.println("Dit efternavn er: " + nameLast);
         System.out.println("Dit fulde navn er: " + name + " " + nameLast);
